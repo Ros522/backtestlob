dfb51fdeccd9f694daf4c485b30aa9ba7d1ec7a2
ros <ros@localhost>
2019-06-15 10:48:14 +0900

    オラァ

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # backtestlob
+Limit Order Book 指値戦略の高速バックテスト
+(on pybind11)
+# 機能
+1. 買い指値に対して、「買い約定履歴 < 指値」　で注文の約定判定を行う 
+2. 売り指値に対して、「売り約定履歴 < 指値」　で注文の約定判定を行う 
+3. 約定したら内部のポジションを更新して、その注文約定の収支を返す
+4. 注文はネットアウト注文として保持する
+5. 安値、高値入力で約定判定(step)、または1約定履歴流し込み(step2)
+6. 成行できない
+
+# イメージ
+```
+from backtest import BackTestEnv
+env = BackTestEnv()
+#1枚買い
+env.entry(0,1,500000)
+#Low,Highで約定判定
+profit,trade = env.step(480000,510000)
+print(f"profit={profit},trade={trade}")
+print(f"position={env.getPositionSize()} side={env.getPositionSide()} price={env.getPositionPrice()}")
+
+#本stepでの収支0,ポジション買い　1枚　平均単価50万
+#profit=0,trade=1
+#position=1.0 side=0.0 price=500000.0
+
+#1枚売り
+env.entry(1,1.5,520000)
+#約定判定
+profit,trade = env.step(510000,530000)
+
+print(f"profit={profit},trade={trade}")
+print(f"position={env.getPositionSize()} side={env.getPositionSide()} price={env.getPositionPrice()}")
+
+#本stepでの収支20000,ポジション売り　0.5枚　平均単価52万
+#profit=20000,trade=1
+#position=0.5 side=1.0 price=520000.0
+```
---
+++ backtest/backtest.cpp
@@ -0,0 +1,161 @@
+﻿// backtest.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+class BackTestEnv {
+	struct Order {
+		int side;
+		float size;
+		float price;
+	};
+private:
+
+	Order position = { -1,0,0 };
+	std::vector<Order> orders;
+
+public:
+	BackTestEnv() {
+	}
+
+	float get_position_side() {
+		return this->position.side;
+	}
+	float get_position_size() {
+		return this->position.size;
+	}
+	float get_position_price() {
+		return this->position.price;
+	}
+
+	float add_position(Order newpos) {
+		float profit = 0;
+		if (this->position.side >= 0) {
+			if (newpos.side == this->position.side) {
+				float sum_size = this->position.size + newpos.size;
+				float newprice = (this->position.size * this->position.price + newpos.size*newpos.price) / sum_size;
+
+				Order newposion = { newpos.side, sum_size, newprice };
+				this->position = newposion;
+			}
+			else if (newpos.side == 0 && this->position.side == 1) {
+				float after_size = this->position.size - newpos.size;
+
+				float ex_size = this->position.size - std::max(0.0f, after_size);
+
+				profit = (this->position.price - newpos.price)*ex_size;
+
+				if (after_size > 0.0f) {
+					Order newposion = { 1,after_size,this->position.price };
+					this->position = newposion;
+				}
+				else if (after_size < 0.0f) {
+					Order newposion = { 0,std::abs(after_size),newpos.price };
+					this->position = newposion;
+				}
+				else {
+					this->position = { -1,0,0 };
+				}
+			}
+			else if (newpos.side == 1 && this->position.side == 0) {
+
+				float after_size = this->position.size - newpos.size;
+
+				float ex_size = this->position.size - std::max(0.0f, after_size);
+
+				profit = (newpos.price- this->position.price)*ex_size;
+
+				if (after_size > 0.0f) {
+					Order newposion = { 0,after_size,this->position.price };
+					this->position = newposion;
+				}
+				else if (after_size < 0.0f) {
+					Order newposion = { 1,std::abs(after_size),newpos.price };
+					this->position = newposion;
+				}
+				else {
+					this->position = { -1,0,0 };
+				}
+			}
+		}
+		else
+		{
+			this->position = newpos;
+		}
+		return profit;
+	}
+
+	std::tuple<float, int> step(float low, float high) {
+		std::vector<Order>::iterator it = this->orders.begin();
+		int trade = 0;
+		float profit = 0;
+		while(it != this->orders.end()) {
+			//約定チェック
+			Order o = *it;
+			if (low < o.price && high > o.price) {
+				//約定している
+				trade++;
+				profit += this->add_position(o);
+				it = this->orders.erase(it);
+			}
+			else
+			{
+				it++;
+			}
+		}
+		return std::forward_as_tuple(profit, trade);
+	}
+
+	std::tuple<float, int> step2(int side, float price) {
+		std::vector<Order>::iterator it = this->orders.begin();
+		int trade = 0;
+		float profit = 0;
+		while (it != this->orders.end()) {
+			//約定チェック
+			Order o = *it;
+			//買い履歴の場合、売り注文を見る
+			if (side == 0 && o.side == 1 && o.price < price) {
+				//約定している
+				trade++;
+				profit += this->add_position(o);
+				it = this->orders.erase(it);
+			}
+			else if (side == 1 && o.side == 0 && o.price > price) {
+			//売り履歴の場合、買い注文を見る
+				trade++;
+				profit += this->add_position(o);
+				it = this->orders.erase(it);
+			}
+			else
+			{
+				it++;
+			}
+		}
+		return std::forward_as_tuple(profit, trade);
+	}
+
+	void entry(int side, double size, float price) {
+		Order o = { side,size,price };
+		orders.push_back(o);
+	};
+	void cancelAll() {
+		orders.clear();
+	};
+};
+
+PYBIND11_MODULE(backtest, m) {
+	m.doc() = "backtest_plugin";
+	py::class_<BackTestEnv>(m, "BackTestEnv")
+		.def(py::init())
+		.def("getPositionSide", &BackTestEnv::get_position_side)
+		.def("getPositionSize", &BackTestEnv::get_position_size)
+		.def("getPositionPrice", &BackTestEnv::get_position_price)
+		.def("step", &BackTestEnv::step)
+		.def("step2", &BackTestEnv::step2)
+		.def("entry", &BackTestEnv::entry)
+		.def("cancelAll", &BackTestEnv::cancelAll)
+		;
+}
